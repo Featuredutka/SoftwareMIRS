5fb1ac7e25b2ca1d32ca6ba307c9f346baac217b
Андрей Шингирий <featuredboogie@gmail.com>
2021-09-21 12:59:22 +0300

    Lab 1

---
+++ Sh_0218_l1/Logical/Program/Cyclic.c
@@ -0,0 +1,24 @@
+
+#include <bur/plctypes.h>
+
+#ifdef _DEFAULT_INCLUDES
+	#include <AsDefault.h>
+#endif
+
+void _CYCLIC ProgramCyclic(void)
+{
+	fb_regulator.e = speed;
+	fb_regulator.e -= fb_motor.w;
+	FB_Regulator(&fb_regulator);
+	fb_motor.u = fb_regulator.u;
+	FB_Motor(&fb_motor);
+	
+	secondmotor.u = speed;
+	FB_Motor(&secondmotor);
+	
+	if (counter >= pwm_period){
+		speed = (speed) ? 0 : 100;
+		counter = 0;
+	}
+	counter++;
+}
